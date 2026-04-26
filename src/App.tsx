824d1c9e7e96ea0702bf04317a9c765316eef5e0
Sai Kiran <saisrikiran_ipm25@iift.edu>
2026-04-27 01:43:43 +0530

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Chat from './Chat';
 import LandingPage from './LandingPage';
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
